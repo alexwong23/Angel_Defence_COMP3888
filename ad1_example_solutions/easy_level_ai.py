1eec6d85d47379c61b18cfe2051a8da061f86ce8
DAIHUI YU <dayu3519@uni.sydney.edu.au>
2020-10-25 14:36:35 +1100

    Added AD1 easy level solution

---
+++ ad1_example_solutions/easy_level_ai.py
@@ -0,0 +1,9 @@
+AVAILABLE = ["archer", "warrior", "wizard"]
+# Goal: make your angel survive longer than the other team.
+#       summon units to protect it!
+# Use game.spawn(<unitType>, <position_number>) to summon your unit.
+# They are "auto chess"! You don't need to control their action.
+game.spawn("archer",0)
+game.spawn("archer",3)
+game.spawn("wizard",2)
+game.spawn("archer",4)
