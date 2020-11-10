63dcf8a57a50127998f3ccfce897fc4d112b414a
Alex Wong <awon6941@uni.sydney.edu.au>
2020-11-11 07:15:03 +0800

    wizard beat archers

--- ad2_player_code/WizardsBeatArcherspy.py
+++ ad2_player_code/WizardsBeatArcherspy.py
@@ -11,39 +11,45 @@
 # TAKE NOTE: Detailed examples of these methods can be found in the documentation. GLHF! :)
 
 # A. SET UNIT BEHAVIOURS HERE
-# warriors attack the nearest enemy
-def warriorSpawnFunction(e):
+# archers attack the nearest enemy
+def archerSpawnFunction(e):
     me = e.target
     while True:
         enemy = me.findNearestEnemy()
-        if enemy:
+        if enemy and enemy.distanceTo(ownAngel) < 20: # enemy close to angel, attack!
+            me.attack(enemy)
+        elif enemy and enemy.distanceTo(ownTower) < 15: # enemy close to tower, run to angel!
+            me.move(ownAngel.pos)
+        elif enemy and me.distanceTo(enemy) < 15: # enemy close to me, run to tower!
+            me.move(ownTower.pos)
+        elif enemy:
             me.attack(enemy)
 
 # B. SET UNIT ACTIONS HERE
 # all warrior units will behave as instructed in the function 'warriorSpawnFunction'
 # add the behaviour into the game.setActionFor user method
-game.setActionFor("warrior", "spawn", warriorSpawnFunction)
+game.setActionFor("wizard", "spawn", archerSpawnFunction)
 
 # C. SOME HELPFUL VARIABLES
-# use these methods to locate units in the game, both friends and enemies
+# use these methods to locate units in the game, both friends and enemies within 60
 enemyHero = hero.findByType("duelist", hero.findEnemies())[0]
 ownTower = hero.findByType("arrow-tower", hero.findFriends())[0]
 ownAngel = hero.findByType("angel-fountain", hero.findFriends())[0]
 
 # D. GAME LOGIC HERE
 while True:
-    # spawn warrior units, one of the eight unique unit types
-    if hero.gold > hero.costOf("warrior"):
-        game.spawn("warrior")
+    # spawn archer units, one of the eight unique unit types
+    if hero.gold > hero.costOf("wizard"):
+        game.spawn("wizard")
 
     # E. HERO LOGIC HERE
     enemy = hero.findNearestEnemy()
     item = hero.findNearestItem()
     # E.1 get potion
-    if item and hero.health < 200:
+    if item and hero.health < 150:
         hero.move(item.pos)
     # E.2 retreat to base
-    elif not item and hero.health < 200:
+    elif not item and hero.health < 150:
         hero.move(ownAngel.pos)
     # E.3 hero attacks nearby enemies
     elif enemy:
